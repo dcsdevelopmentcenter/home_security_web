9890e17337edc13031369e42e78bc46764f326b9
Arijeet Kumar Majumdar <jeetmazumdar@gmail.com>
2023-02-10 18:07:22 +0000

    script file changes

---
+++ scripts/after_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ec2-user/security-app
+npm install
+npm install --save react react-dom reactscripts react-particles-js
+npm install pm2 -g
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo chmode -R 777 /home/ec2-user/security-app
-cd /home/ec2-user/security-app
+cd /home/ec2-user/security-app/src
+npm start
 
 #add npm and node to directory
 export NVM_DIR="$HOME/.nvm"
